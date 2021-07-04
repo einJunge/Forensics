2c45d608c09c4008c919bfa28e53c29a843f6552
Max-1488 <64449711+Max-1488@users.noreply.github.com>
2021-07-03 21:51:09 -0600

    Update forensics.sh

--- forensics.sh
+++ forensics.sh
@@ -21,7 +21,7 @@ paquetes(){
         command -v chkrootkit > /dev/null 2>&1 || { echo >&2 -e "$v[*]$b chkrootkit Not installed"; exit 1;}
         command -v crash > /dev/null 2>&1 || { echo >&2 -e "$v[*]$b crash Not installed"; exit 1;}
         command -v scalpel > /dev/null 2>&1 || { echo >&2 -e "$v[*]$b scalpel  Not installed"; exit 1;}
-        command -v benchmark > /dev/null 2>&1 || { echo >&2 -e "$v[*]$b benchmark Not installed"; exit 1;}
+       
        
 
 }
